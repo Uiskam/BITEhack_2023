a1a4922e631d355bc6af4d86163458c9e9ad437b
jchyczynski <jasiek.ch16@gmail.com>
2023-01-15 00:49:11 +0100

    languages dropdown, screen manager

--- src/gui/path_chooser.py
+++ src/gui/path_chooser.py
@@ -1,10 +1,17 @@
 from kivy.lang import Builder
+from kivy.metrics import dp
 from kivy.properties import StringProperty
+from kivy.uix.screenmanager import Screen
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivy.core.window import Window
+from kivymd.uix.list import OneLineIconListItem
+from kivymd.uix.menu import MDDropdownMenu
+
+Builder.load_file('gui/path_chooser.kv')
+Builder.load_file('gui/input_files_screen.kv')
 
 
-class InputFilesScreen(MDBoxLayout):
+class InputFilesScreen(Screen):
     def on_file_drop(self, window, file_path, x, y):
         print("file dropped, im InputFilesScreen")
         self.ids.video_chooser.on_file_drop(window, file_path, x, y)
@@ -19,5 +26,38 @@ class PathChooser(MDBoxLayout):
             self.ids.input_field.text = file_path
 
 
-Builder.load_file('gui/path_chooser.kv')
-Builder.load_file('gui/input_files_screen.kv')
+class IconListItem(OneLineIconListItem):
+    icon = StringProperty()
+
+
+class LanguagesChooser(MDBoxLayout):
+    file_type = StringProperty("file_type")
+    supported_languages = ['Spanish', 'English']
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        menu_items = [
+            {
+                "viewclass": "IconListItem",
+                "icon": "git",
+                "text": lang,
+                "height": dp(56),
+                "on_release": lambda x=lang: self.set_item(x),
+            } for lang in self.supported_languages
+        ]
+        print("Langage Chooser init")
+        self.menu = MDDropdownMenu(
+            caller=self,
+            items=menu_items,
+            position="center",
+            width_mult=4,
+        )
+        # self.menu.bind()
+
+    def set_item(self, text_item):
+        self.ids.drop_item.set_item(text_item)
+        self.menu.dismiss()
+
+    def open_menu(self):
+        # self.menu.caller = caller
+        self.menu.open()
