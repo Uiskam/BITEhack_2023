322163ff53b86ba475d6653c7e41e73c262f4c1f
mjste <mj.stefanik@gmail.com>
2023-01-14 21:44:36 +0100

    added text quoting

--- src/translate_api.py
+++ src/translate_api.py
@@ -6,6 +6,7 @@ def translate(text='Hello, world!', source_language='en', destination_language='
     '''Translate text from source_language to destination_language. Language codes are ISO 639-1 codes.'''
 
     url = f'http://{ip}:5000/translate'
+    text = requests.utils.quote(text)
     params = {'text': text, 'src': source_language, 'dst': destination_language}
     response = requests.get(url, params=params)
     return json.loads(response.text)
