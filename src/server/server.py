d655d391334d231f29962b7f6fd5183a74ac4178
mjste <mj.stefanik@gmail.com>
2023-01-15 10:35:36 +0100

    added server

---
+++ src/server/server.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+from flask import Flask, request
+from translate_proxy import translate_text
+import json
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def greet():
+    return 'enter http://IP:5000/translate?text=TEXT to translate from english to spanish'
+
+
+@app.route('/translate', methods=['GET'])
+def translate():
+    try:
+        text = request.args.get('text')
+        source_language=request.args.get('src')
+        destination_language=request.args.get('dst')
+    except KeyError as e:
+        return 'Incorrect parameters. Please enter http://IP:5000/translate?text=TEXT&src=en&dst=es to translate from english to spanish'
+
+    text = requests.utils.unquote(text)
+
+    results = translate_text(text, source_language, destination_language)
+
+    return json.dumps(results)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
