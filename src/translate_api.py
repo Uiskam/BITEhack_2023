3e63f96f78e8eef3407fc860e645cc3462441dc8
mjste <mj.stefanik@gmail.com>
2023-01-15 09:59:16 +0100

    fix lack of running server ;)

--- src/translate_api.py
+++ src/translate_api.py
@@ -1,6 +1,7 @@
 import requests
 import json
 
+failed_retries = 0
 
 def translate(text: str = 'Hello, world!',
               source_language: str = 'en',
@@ -8,8 +9,22 @@ def translate(text: str = 'Hello, world!',
               ip: str = '192.168.52.98') -> object:
     """Translate text from source_language to destination_language. Language codes are ISO 639-1 codes."""
 
+    global failed_retries
     url = f'http://{ip}:5000/translate'
     text = requests.utils.quote(bytes(text))
     params = {'text': text, 'src': source_language, 'dst': destination_language}
-    response = requests.get(url, params=params)
-    return json.loads(response.text)
+    if failed_retries < 3:
+        try:
+            response = requests.get(url, params=params)
+            response_text = response.text
+        # print(response_text)
+        # print(type(response_text))
+            failed_retries = 0
+            return response_text
+        except requests.exceptions.ConnectionError as e:
+            print(e)
+            failed_retries += 1
+            return 'Nie ma połączenia'
+    else:
+        return 'Nie ma połączenia'
+
